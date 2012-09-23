46516f8da58c6fdb5b749c3847901d0932c0da23
Alexander Biggs <spirit.of.mana@gmail.com>
2012-09-22 21:33:17 -0400

    Tree stuff.

--- BabbleEngine/BabbleEngine/RoomObjects/Tree.cs
+++ BabbleEngine/BabbleEngine/RoomObjects/Tree.cs
@@ -8,15 +8,16 @@ namespace BabbleEngine.RoomObjects
 {
     public class Tree : WorldObject
     {
-        // minimum number of branches
-        const int MIN_BRANCHES = 4;
-        const int MAX_BRANCHES = 10;
-
         // how far off the ground branches can start growing
         // from the tree
         const float BRANCH_STARTING_HEIGHT = 20;
+        
+        // how much branches can be rotated by
+        const float BRANCH_ROTATION_MIN = 0;
+        const float BRANCH_ROTATION_MAX = MathHelper.Pi / 4;
 
         public float Height;
+        float rotation;
         List<Tree> branches = new List<Tree>();
 
         /// <summary>
@@ -24,26 +25,33 @@ namespace BabbleEngine.RoomObjects
         /// </summary>
         /// <param name="Position">The starting position of the tree.</param>
         /// <param name="Room">The room in which the tree is located.</param>
-        public Tree(Vector2 Position, float Height, Room Room, float Rotation=0)
+        public Tree(Vector2 Position, float Height, float numBranches, Room Room, float Rotation=0)
             : base(Position, Vector2.Zero, TextureBin.GetTexture("Objects/tree"), Room)
         {
             this.Height = Height;
 
             // add in a random number of branches
             Random random = new Random();
-            int numBranches = random.Next(MIN_BRANCHES, MAX_BRANCHES);
             float branchSeparationDistance = (Height - BRANCH_STARTING_HEIGHT) / numBranches; 
 
             for (int i = 0; i < numBranches; i++)
             {
                 // the side of the tree the branch is located on
-                int facing = random.Next(0, 1) == 1 ? 1 : -1;
-                
+                // 1 for left, 0 for right.
+                int facing = random.Next(0, 1);
+
+                Vector2 branchPosition = new Vector2(facing == 1 ? Position.X : Position.X + texture.Width,
+                    i * branchSeparationDistance + BRANCH_STARTING_HEIGHT);
+                branches.Add(new Tree(branchPosition, Height / 4, numBranches / 4, Room,
+                    rotation + (float)(facing == 1 ? BRANCH_ROTATION_MIN + MathHelper.TwoPi - MathHelper.PiOver2 + random.NextDouble() * BRANCH_ROTATION_MAX :
+                                                BRANCH_ROTATION_MIN + MathHelper.PiOver2 + random.NextDouble() * BRANCH_ROTATION_MAX)));
             }
         }
 
         public override void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
         {
+            foreach (Tree branch in branches)
+                branch.Draw(spriteBatch);
             DrawHelper.DrawLine(spriteBatch, position, new Vector2(position.X, position.Y - Height), Color.Brown);
         }
     }
